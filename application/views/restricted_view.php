799912c503c3689098c350b478d8d53382ad49c5
texasag99 <bejan.nouri@gmail.com>
2014-05-13 19:36:03 -0400

    Added restricted_view.php file to repository

---
+++ application/views/restricted_view.php
@@ -0,0 +1,17 @@
+<?php include("header.php"); ?>
+
+<div id="container">
+<h1>Restricted!</h1>
+
+<div id="body">
+ <p style="max-width:800px; border:1px solid dark-gray; background-color:silver; padding:10px;">
+  This page is RESTRICTED! </p>
+ <p>You have attempted to access a page that is restricted. <a href='<?php echo base_url()."main/login"; ?>'>Please click here to login</a>
+</p>
+</div>
+</div>
+
+</body>
+</html>
+
+
